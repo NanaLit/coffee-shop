9b0577d45af7f4b60c8ececa39b25fe12c0b750b
Nana <zubiloandphima@gmail.com>
2022-07-08 14:07:00 +0300

    Закончила главную страницу

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -1,10 +1,12 @@
 import './footer.scss'
 import Hat from '../hat/hat';
+import Divider from '../divider/divider';
 
 function Footer() {
     return (
         <footer className="footer">
             <Hat footer={true}/>
+            <Divider color={true}/>
         </footer>
     )
 }
--- src/Components/hat/hat.js
+++ src/Components/hat/hat.js
@@ -5,20 +5,21 @@ import logoBlack from './logo-black.png';
 function Hat({footer}) {
 
     let img = logo;
+    let clazz = "hat__item";
 
-    footer ? (img = logoBlack) : (img = logo);
+    footer ? ((img = logoBlack) && (clazz = "hat__item hat__item_black")) : ((img = logo) && (clazz = "hat__item"));
 
     return (
         <div className="hat">
             <img className="logo" src={img} alt="logo" />
             <ul className="hat__list">
-                <li className="hat__item">
+                <li className={clazz}>
                     <a href="#">Coffee house</a>
                 </li>
-                <li className="hat__item">
+                <li className={clazz}>
                     <a href="#">Our coffee</a>
                 </li>
-                <li className="hat__item">
+                <li className={clazz}>
                     <a href="#">For your pleasure</a>
                 </li>
             </ul>
