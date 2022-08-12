339acbbe6e7cb38f50181780e9e6bc87e02e1ad2
NanaLit <nanalit16@gmail.com>
2022-08-12 13:37:19 +0300

    пробный

--- src/Components/about/about.js
+++ src/Components/about/about.js
@@ -4,6 +4,7 @@ import Divider from '../divider/divider';
 import Description from '../description/descr';
 
 function AboutUs () {
+    //push
     return (
         <section className="about">
             <div className="container">
